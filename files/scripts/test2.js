b7f1585cb0392c704a13b4800fbc77ed6e080a21
oskarbukovsky <47369119+oskarbukovsky@users.noreply.github.com>
2022-07-12 13:39:43 +0200

    update

--- files/scripts/test2.js
+++ files/scripts/test2.js
@@ -122,48 +122,6 @@ function PlayerControll(event) {
 
 }
 
-/*def PlayerControll(){
-    # X analog 1 axis
-    xAnalog1 = document.getElementById('xAnalog1').value - 0.5
-    if (abs(xAnalog1) > deathZone):
-        if (circle.attr('cx') + xAnalog1 * moveSpeed > circle.attr('r') and circle.attr('cx') + xAnalog1 * moveSpeed < paper.width - circle.attr('r')):
-    circle.attr('cx', circle.attr('cx') + xAnalog1 * moveSpeed)
-            # circle.translate(xAnalog1 * moveSpeed, 0)
- 
-    # Y analog 1 axis
-    yAnalog1 = document.getElementById('yAnalox1').value - 0.5
-    if (abs(yAnalog1) > deathZone):
-        if (circle.attr('cy') + yAnalog1 * moveSpeed > circle.attr('r') and circle.attr('cy') + yAnalog1 * moveSpeed < paper.height - circle.attr('r')):
-    circle.attr('cy', circle.attr('cy') + yAnalog1 * moveSpeed)
-            # circle.translate(0, yAnalog1 * moveSpeed)
- 
-    # X analog 2 axis
-    xAnalog2 = document.getElementById('xAnalog2').value - 0.5
-    if (abs(xAnalog2) > deathZone * 4):
-        bullet = paper.circle(circle.attr('cx'), circle.attr('cy'), 5)
-    bullet.attr("fill", "#0f0")
-    bullet.attr("stroke", "#fff")
-        # bullet.attr('cx', circle.attr('cx') + xAnalog2 * moveSpeed)
- 
-    bullet.animate({
-        'cx': circle.attr('cx') + xAnalog2 *
-            100000
-    }, 100000 / bulletSpeed, 'linear')
- 
-    # Y analog 2 axis
-    yAnalog2 = document.getElementById('yAnalog2').value - 0.5
-    if (abs(yAnalog2) > deathZone * 4):
-        bullet = paper.circle(circle.attr('cx'), circle.attr('cy'), 5)
-    bullet.attr("fill", "#0f0")
-    bullet.attr("stroke", "#fff")
-    #bullet.attr('cy', circle.attr('cy') + yAnalog2 * moveSpeed)
- 
-    bullet.animate({
-        'cy': circle.attr('cy') + yAnalog2 *
-            100000
-    }, 100000 / bulletSpeed, 'linear')
-}*/
-
 window.addEventListener('gamepadconnected', (event) => {
     for (const gamepad of navigator.getGamepads()) {
         if (!gamepad) continue;
