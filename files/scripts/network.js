04ad8eaf8a7e119414ec37ef87bf6ad1a7519971
oskarbukovsky <47369119+oskarbukovsky@users.noreply.github.com>
2022-07-14 18:38:40 +0200

    working chat

--- files/scripts/network.js
+++ files/scripts/network.js
@@ -13,6 +13,9 @@ class Message {
     }
 }
 
+let messages = [];
+let activeUsersLastMessage = []
+
 var nameList = [
     'Time', 'Past', 'Future', 'Dev',
     'Fly', 'Flying', 'Soar', 'Soaring', 'Power', 'Falling',
@@ -58,7 +61,48 @@ function getRandomIntInclusive(min, max) {
 function updateMembers() {
     $("#members-count").html("Members: " + members.length);
     $("#members-list").html("");
-    members.forEach(member => { $("#members-list").append(member.clientData.username + "<br>") });
+    members.forEach(member => { $("#members-list").append("<span onclick='connectTo(this.id)' id='" + member.clientData.uuid + "'>" + member.clientData.username + "</span><br>") });
+}
+
+let updateMessages = () => {
+    messages = messages.filter((msg) => { return members.some((member) => { return member.clientData.uuid == JSON.parse(msg.message).uuid }) });
+    //messages = messages.filter((msg) => { JSON.parse(msg.message).uuid != uuid });
+
+
+    //fix new Set(messages.map( msg => { return JSON.parse(msg.message).uuid})).forEach( u_uuid => { messages.map( msg => { return JSON.parse(msg.message)}).filter( msg => { return msg.uuid == u_uuid }) });
+
+
+
+    //
+    //  messages.map( msg => { return JSON.parse(msg.message).timestamp});
+    //  Math.max(...messages.map( msg => { return JSON.parse(msg.message).timestamp}));
+    //  new Set(messages.map( msg => { return JSON.parse(msg.message).uuid}))
+    //
+    activeUsersLastMessage = [];
+    Array.from(new Set(messages.map(msg => { return JSON.parse(msg.message).uuid }))).forEach(function callb(uUuid, index) { activeUsersLastMessage.push(new Array()); messages.map(msg => { return JSON.parse(msg.message) }).filter(msg => { if (msg.uuid == uUuid) { activeUsersLastMessage[index].push(msg) } }) });
+    activeUsersLastMessage = activeUsersLastMessage.map((msgs) => { return msgs.filter((msg) => { return msg.timestamp == Math.max(...msgs.map(msg => { return msg.timestamp })) }) }).map(msg => { return msg[0] });
+    //
+    //  activeUsersLastMessage.forEach((msgs) => {msgs.map(
+    //
+    //  Math.max(...activeUsersLastMessage[0].map( msg => { return msg.timestamp}));
+    //
+    //  activeUsersLastMessage[0].filter((msg) => { return msg.timestamp == Math.max(...activeUsersLastMessage[0].map( msg => { return msg.timestamp}));})
+    //
+    $("#" + uuid).css('color', 'blue')
+    members.forEach(member => { if (activeUsersLastMessage.some((msg) => { return msg.uuid == member.clientData.uuid })) $("#" + member.clientData.uuid).css('color', 'green') })
+
+}
+
+function connectTo(remoteUuid) {
+    if ($("#" + remoteUuid).css('color') == 'rgb(0, 128, 0)') {
+        console.log("Connecting to " + remoteUuid);
+        webRTC.remoteOffer = activeUsersLastMessage.filter((msg) => { return msg.uuid == remoteUuid })[0].message;
+        onRecievedOffer();
+    }
+}
+
+function msgToLog(user, message){
+    console.log('[' + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds() + '][' + user + ']: ' + message);
 }
 
 let uuid = uuidv4();
@@ -66,14 +110,39 @@ let username = prompt("Enter your name:") ?? randName();
 let remoteUsername;
 let drone = new Scaledrone('9lSwhGfWeyzdNMl5', {
     data: {
-        username: username
+        username: username,
+        uuid: uuid
     }
 });
+
+function isJsonString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
 let room;
 var members = [];
 var cfg = { 'iceServers': [{ 'url': "stun:stun.gmx.net" }] };
 var con = { 'optional': [{ 'DtlsSrtpKeyAgreement': true }] };
-var peerConnection1 = new RTCPeerConnection(cfg, con), dataChannel1 = null, tn1 = null, activedc, peerConnection1icedone = false;
+var peerConnection1;
+var activedc;
+function newPeerConnection1() {
+    peerConnection1 = new RTCPeerConnection(cfg, con), dataChannel1 = null, tn1 = null, activedc = null, peerConnection1icedone = false;
+    peerConnection1.onicecandidate = function (e) {
+        if (e.candidate == null) {
+
+            //Broadcast offer to room
+            drone.publish({
+                room: 'observable-room',
+                message: JSON.stringify(new Message(username, uuid, peerConnection1.localDescription))
+            });
+        }
+    }
+}
 var peerConnection2 = new RTCPeerConnection(cfg, con), dataChannel2 = null, peerConnection2icedone = false;
 let webRTC = {
     localOffer: "",
@@ -82,13 +151,12 @@ let webRTC = {
     localAnswer: ""
 }
 
-
 drone.on('open', () => {
     drone.on('error', (error) => {
         console.error('Error with connection:', error);
     });
     drone.on('close', (event) => {
-        console.log('Connection closed:', event);
+        console.warn('Connection closed:', event);
     });
 
     room = drone.subscribe('observable-room', {
@@ -99,18 +167,27 @@ drone.on('open', () => {
             console.error(error);
         } else {
             console.log('Connected to room');
+            //Check if message is valid json and put those messages to filtered history
+            messages = room._history.messages.filter((msg) => { try { filter = JSON.parse(msg.message).uuid } catch (e) { return false } return filter || false });
+            updateMessages();
+            createOffer();
         }
     });
 
     room.on('members', memberList => {
         members = memberList;
-
+        updateMessages();
         updateMembers();
-    });
+
+    })
+
+    updateMembers();
 
     room.on('member_join', member => {
         members.push(member);
+        console.log("Members: " + member.clientData.username + " joined");
 
+        updateMessages();
         updateMembers();
     });
 
@@ -120,18 +197,26 @@ drone.on('open', () => {
         console.log("Members: " + members[index].clientData.username + " left");
         members.splice(index, 1);
 
+        updateMessages();
         updateMembers();
     });
 
     room.on('message', message => {
         if (JSON.parse(message.data).uuid != uuid) {
-            console.log(JSON.parse(message.data));
+            messages.push(new Object({
+                timestamp: JSON.parse(message.data).timestamp,
+                message: message.data
+            }));
+            updateMessages();
+        }
+        if (JSON.parse(message.data).uuid != uuid) {
             if (JSON.parse(message.data).message.type == "offer") {
                 webRTC.remoteOffer = JSON.stringify(JSON.parse(message.data).message);
             }
 
             if (JSON.parse(message.data).message.type == "answer") {
                 webRTC.remoteAnswer = JSON.stringify(JSON.parse(message.data).message);
+                onRecievedAnswer();
             }
         }
 
@@ -139,17 +224,16 @@ drone.on('open', () => {
 });
 
 
-
 // BOB: pasted Alice's answer
-answerRecdBtn.onclick = function () {
+let onRecievedAnswer = function () {
     var answerDesc = new RTCSessionDescription(JSON.parse(webRTC.remoteAnswer))
     peerConnection1.setRemoteDescription(answerDesc);
 };
 
 // ALICE: pasted Bob's answer
-offerRecdBtn.onclick = function () {
-    var offerDesc = new RTCSessionDescription(JSON.parse(webRTC.remoteOffer))
-    peerConnection2.setRemoteDescription(offerDesc)
+let onRecievedOffer = function () {
+    var offerDesc = new RTCSessionDescription(webRTC.remoteOffer);
+    peerConnection2.setRemoteDescription(offerDesc);
     peerConnection2.createAnswer(function (answerDesc) {
         peerConnection2.setLocalDescription(answerDesc)
     },
@@ -162,14 +246,15 @@ function sendMessage() {
         //Print msg on local terminal
 
         activedc.send(JSON.stringify(new Message(username, uuid, messageTextBox.value)));
-        chatlog.innerHTML += '[' + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds() + '][' + username + ']: ' + messageTextBox.value + '</p>';
+        msgToLog(username, messageTextBox.value);
         messageTextBox.value = "";
     }
     return false
 }
 
 // BOB: create
-createBtn.onclick = function () {
+let createOffer = function () {
+    newPeerConnection1();
     dataChannel1 = peerConnection1.createDataChannel('test', { reliable: true })
     activedc = dataChannel1
     dataChannel1.onopen = function (e) { }
@@ -186,9 +271,7 @@ createBtn.onclick = function () {
             if (data.type === 'file') {
                 fileReceiver1.receive(e.data, {})
             } else {
-                console.log(data);
-                chatlog.innerHTML += '[' + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds() + '][' + data.username + ']: ' + data.message + '</p>';
-                chatlog.scrollTop = chatlog.scrollHeight
+                msgToLog(data.username, data.message);
             }
         }
     }
@@ -212,24 +295,13 @@ peerConnection2.ondatachannel = function (e) {
             if (data.type === 'file') {
                 fileReceiver2.receive(e.data, {})
             } else {
-                console.log(data);
-                chatlog.innerHTML += '[' + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds() + '][' + data.username + ']: ' + data.message + '</p>';
-                chatlog.scrollTop = chatlog.scrollHeight;
+                msgToLog(data.username, data.message);
             }
         }
     }
 }
 
-peerConnection1.onicecandidate = function (e) {
-    if (e.candidate == null) {
 
-        //Broadcast offer to room
-        drone.publish({
-            room: 'observable-room',
-            message: JSON.stringify(new Message(username, uuid, peerConnection1.localDescription))
-        });
-    }
-}
 peerConnection2.onicecandidate = function (e) {
     if (e.candidate == null) {
 
--- server.bat
+++ server.bat
@@ -1,3 +1,5 @@
 @echo off
 title Server
+start /min cmd.exe /C "code . > Nul"
+start http://127.0.0.1.nip.io
 python -m http.server 80
