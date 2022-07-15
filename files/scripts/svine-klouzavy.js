e90ee1640273c8d6eda08961c0623cec1bf042b4
oskarbukovsky <47369119+oskarbukovsky@users.noreply.github.com>
2022-07-15 14:57:28 +0200

    migration to phaser

--- files/scripts/svine-klouzavy.js
+++ files/scripts/svine-klouzavy.js
@@ -1,14 +1,50 @@
+; (function (Raphael) {
+    /// Plugin - replaces original RaphaelJS .image constructor
+    /// with one that respects original dimensions.
+    /// Optional overrides for each dimension.
+    /// @drzaus @zaus
+    /// based on http://stackoverflow.com/questions/10802702/raphael-js-image-with-its-original-width-and-height-size
+
+    var originalRaphaelImageFn = Raphael.fn.image;
+    /**
+     * Overload - replace Raphael .image function with one that respects original dimensions
+     * @param  {string} url the image path
+     * @param  {number} x   position-x
+     * @param  {number} y   position-y
+     * @param  {number} w   dimensions-width
+     * @param  {number} h   dimensions-height
+     * @return {Paper.Element}     new raphael element
+     */
+    Raphael.fn.image = function (url, x, y, w, h) {
+        // fix the image dimensions to match original scale unless otherwise provided
+        if (!w || !h) {
+            var img = new Image();
+            img.src = url;
+            if (!w) w = img.width;
+            if (!h) h = img.height;
+        }
+        return originalRaphaelImageFn.call(this, url, x, y, w, h);
+    };
+    Raphael.st.setID = function (id) {
+        this.id = id;
+    }
+})(Raphael);
+
+
 // Creates canvas 320 × 200 at 10, 50
 paper = window.Raphael(210, 210, 640, 480);
 
+
 // Creates circle at x = 50, y = 40, with radius 10
-circle1 = paper.circle(40, 240, 10);
-circle1.attr("fill", "#0f0");
-circle1.attr("stroke", "#fff");
+//circle1 = paper.circle(40, 240, 10);
+//circle1.attr("fill", "#0f0");
+//circle1.attr("stroke", "#fff");
 
 circle2 = paper.circle(600, 240, 10);
 circle2.attr("fill", "#0f0");
 circle2.attr("stroke", "#fff");
+
+circle1 = paper.image('files/img/blue-pusher.png', 40, 240, 80, 80);
 
 standartLayout = ["xAnalog1", "yAnalog1", "xAnalog2", "yAnalog2"];
 let toSend = {};
@@ -26,61 +62,63 @@ function PlayerControll(event) {
     // X analog 1 axis
     xAnalog1 = $("#0-xAnalog1").val() - 0.5;
     if (Math.abs(xAnalog1) > deathZone) {
-        if (circle1.attr('cx') + xAnalog1 * moveSpeed > circle1.attr('r') && circle1.attr('cx') + xAnalog1 * moveSpeed < (paper.width / 2) - circle1.attr('r')) {
-            circle1.attr('cx', circle1.attr('cx') + xAnalog1 * moveSpeed);
+        if (circle1.attr('x') + xAnalog1 * moveSpeed > circle1.attr('r') && circle1.attr('x') + xAnalog1 * moveSpeed < (paper.width / 2) - circle1.attr('r')) {
+            circle1.attr('x', circle1.attr('x') + xAnalog1 * moveSpeed);
         } else if (circle1.attr('cx') + xAnalog1 * moveSpeed < circle1.attr('r')) {
-            circle1.attr('cx', circle1.attr('r'));
+            circle1.attr('x', circle1.attr('r'));
         } else {
-            circle1.attr('cx', (paper.width / 2) - circle1.attr('r'));
+            circle1.attr('x', (paper.width / 2) - circle1.attr('r'));
         }
     }
 
     // Y analog 1 axis
     yAnalog1 = $("#0-yAnalog1").val() - 0.5;
     if (Math.abs(yAnalog1) > deathZone) {
-        if (circle1.attr('cy') + yAnalog1 * moveSpeed > circle1.attr('r') && circle1.attr('cy') + yAnalog1 * moveSpeed < paper.height - circle1.attr('r')) {
-            circle1.attr('cy', circle1.attr('cy') + yAnalog1 * moveSpeed);
-        } else if (circle1.attr('cy') + yAnalog1 * moveSpeed < circle1.attr('r')) {
-            circle1.attr('cy', circle1.attr('r'));
+        if (circle1.attr('y') + yAnalog1 * moveSpeed > circle1.attr('r') && circle1.attr('y') + yAnalog1 * moveSpeed < paper.height - circle1.attr('r')) {
+            circle1.attr('y', circle1.attr('y') + yAnalog1 * moveSpeed);
+        } else if (circle1.attr('y') + yAnalog1 * moveSpeed < circle1.attr('r')) {
+            circle1.attr('y', circle1.attr('r'));
         } else {
-            circle1.attr('cy', paper.height - circle1.attr('r'));
+            circle1.attr('y', paper.height - circle1.attr('r'));
         }
     }
 
     // X analog 2 axis
     xAnalog2 = $("#0-xAnalog2").val() - 0.5;
     if (Math.abs(xAnalog2) > deathZone) {
-        if (circle1.attr('cx') + xAnalog2 * moveSpeed > circle1.attr('r') && circle1.attr('cx') + xAnalog2 * moveSpeed < (paper.width / 2) - circle1.attr('r')) {
-            circle1.attr('cx', circle1.attr('cx') + xAnalog2 * moveSpeed);
-        } else if (circle1.attr('cx') + xAnalog2 * moveSpeed < circle1.attr('r')) {
-            circle1.attr('cx', circle1.attr('r'));
+        if (circle1.attr('x') + xAnalog2 * moveSpeed > circle1.attr('r') && circle1.attr('x') + xAnalog2 * moveSpeed < (paper.width / 2) - circle1.attr('r')) {
+            circle1.attr('x', circle1.attr('x') + xAnalog2 * moveSpeed);
+        } else if (circle1.attr('x') + xAnalog2 * moveSpeed < circle1.attr('r')) {
+            circle1.attr('x', circle1.attr('r'));
         } else {
-            circle1.attr('cx', (paper.width / 2) - circle1.attr('r'));
+            circle1.attr('x', (paper.width / 2) - circle1.attr('r'));
         }
     }
 
     // Y analog 2 axis
     yAnalog2 = $("#0-yAnalog2").val() - 0.5;
     if (Math.abs(yAnalog2) > deathZone) {
-        if (circle1.attr('cy') + yAnalog2 * moveSpeed > circle1.attr('r') && circle1.attr('cy') + yAnalog2 * moveSpeed < paper.height - circle1.attr('r')) {
-            circle1.attr('cy', circle1.attr('cy') + yAnalog2 * moveSpeed);
-        } else if (circle1.attr('cy') + yAnalog2 * moveSpeed < circle1.attr('r')) {
-            circle1.attr('cy', circle1.attr('r'));
+        if (circle1.attr('y') + yAnalog2 * moveSpeed > circle1.attr('r') && circle1.attr('y') + yAnalog2 * moveSpeed < paper.height - circle1.attr('r')) {
+            circle1.attr('y', circle1.attr('y') + yAnalog2 * moveSpeed);
+        } else if (circle1.attr('y') + yAnalog2 * moveSpeed < circle1.attr('r')) {
+            circle1.attr('y', circle1.attr('r'));
         } else {
-            circle1.attr('cy', paper.height - circle1.attr('r'));
+            circle1.attr('y', paper.height - circle1.attr('r'));
         }
     }
 
 
 
     //Remote gamepad
+
+    /*
     // X analog 1 axis
     xAnalog1 = $("#1-xAnalog1").val() - 0.5;
     if (Math.abs(xAnalog1) > deathZone) {
         if (circle2.attr('cx') - xAnalog1 * moveSpeed > circle2.attr('r') + (paper.width / 2) && circle2.attr('cx') - xAnalog1 * moveSpeed < paper.width - circle2.attr('r')) {
             circle2.attr('cx', circle2.attr('cx') - xAnalog1 * moveSpeed);
-        } else if (circle2.attr('cx') + xAnalog1 * moveSpeed < circle2.attr('r')) {
-            circle2.attr('cx', (paper.width / 2) - circle1.attr('r'));
+        } else if (circle2.attr('cx') - xAnalog1 * moveSpeed < circle2.attr('r') + (paper.width / 2)) {
+            circle2.attr('cx', (paper.width / 2) + circle2.attr('r'));
         } else {
             circle2.attr('cx', paper.width - circle2.attr('r'));
         }
@@ -103,8 +141,8 @@ function PlayerControll(event) {
     if (Math.abs(xAnalog2) > deathZone) {
         if (circle2.attr('cx') - xAnalog2 * moveSpeed > circle2.attr('r') + (paper.width / 2) && circle2.attr('cx') - xAnalog2 * moveSpeed < paper.width - circle2.attr('r')) {
             circle2.attr('cx', circle2.attr('cx') - xAnalog2 * moveSpeed);
-        } else if (circle2.attr('cx') + xAnalog2 * moveSpeed < circle2.attr('r')) {
-            circle2.attr('cx', (paper.width / 2)-circle2.attr('r'));
+        } else if (circle2.attr('cx') - xAnalog2 * moveSpeed < circle2.attr('r') + (paper.width / 2)) {
+            circle2.attr('cx', (paper.width / 2) + circle2.attr('r'));
         } else {
             circle2.attr('cx', paper.width - circle2.attr('r'));
         }
@@ -120,7 +158,7 @@ function PlayerControll(event) {
         } else {
             circle2.attr('cy', paper.height - circle2.attr('r'));
         }
-    }
+    }*/
 }
 
 window.addEventListener('gamepadconnected', (event) => {
@@ -152,6 +190,8 @@ function onGamepadUpdate(event) {
         indexGamepad++;
     }
     if (activedc?.readyState == "open" || false) {
+        Object.assign(toSend, { "cx": circle1.attr('cx') });
+        Object.assign(toSend, { "cy": circle1.attr('cy') });
         sendMessage(JSON.stringify(toSend));
     };
     PlayerControll(event)
@@ -164,6 +204,8 @@ function msgToLog(user, message) {
     for (const gamepad of navigator.getGamepads()) {
         if (!gamepad) continue;
         for (const [indexAxes, axis] of gamepad.axes.entries()) {
+            circle2.attr('cx', paper.width - parsedData.cx);
+            circle2.attr('cy', parsedData.cy);
             $('#' + (indexGamepad + 1) + "-" + standartLayout[indexAxes]).val(parsedData[(indexGamepad + 1) + "-" + standartLayout[indexAxes]]);
             //console.log($('#' + (indexGamepad + 1) + "-" + standartLayout[indexAxes]));
 
